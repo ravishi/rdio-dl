e5317f939c98c4993624171d4ec7da010acd9649
Dirley Rodrigues <dirleyrls@gmail.com>
2015-11-24 17:08:04 -0200

    Add --verbose option

--- rdio_dl/cli.py
+++ rdio_dl/cli.py
@@ -15,16 +15,17 @@ def add_info_extractor_above_generic(ydl, ie):
 @click.command()
 @click.option(u'-u', u'--user', help=u'A Rdio user')
 @click.option(u'-p', u'--password', help=u'The password')
-@click.option(u'-q', u'--quality',
+@click.option(u'-t', u'--quality',
               type=click.Choice([u'high', u'very-high']),
               default=u'high',
               help=(u'The desired quality of the song.'
                     u' high is 192kbps mp3, very-high is'
                     u' 320kbps mp4. Default is high.'))
+@click.option(u'-v', u'--verbose', 'verbose', flag_value=True, default=False)
 @click.argument(u'urls', required=True, nargs=-1)
-def main(user, password, urls, quality):
+def main(user, password, urls, quality, verbose):
     storage = storage_load()
-    with youtube_dl.YoutubeDL() as ydl:
+    with youtube_dl.YoutubeDL(params=dict(verbose=verbose)) as ydl:
         add_info_extractor_above_generic(
             ydl, RdioIE(storage, user, password, quality=quality))
         ydl.download(urls)
